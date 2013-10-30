2a36c5449228547b1e94ba54d98ab73d88860e29
Maico Timmerman <maico.timmerman@gmail.com>
2013-10-30 13:33:32 +0100

    Created ggd() and test prints

--- python/Ggd.py
+++ python/Ggd.py
@@ -1,2 +1,9 @@
 def ggd(a, b):
-    
+    while b != 0:
+        a,b = b, a % b
+    return a
+
+print 'De ggd van 5 en 15: {}'.format(ggd(5, 15))
+print 'De ggd van 15 en 5: {}'.format(ggd(15, 5))
+print 'De ggd van 16 en 2048: {}'.format(ggd(16, 2048))
+print 'De ggd van 1234 en 81242: {}'.format(ggd(1234, 81247))
