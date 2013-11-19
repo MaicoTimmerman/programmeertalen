75f4d3269df2e74e52ffda7ad67622ed809e833c
Maico Timmerman <maico.timmerman@gmail.com>
2013-11-19 14:25:34 +0100

    Added power function.

--- erlang/testprograms/yet_again.erl
+++ erlang/testprograms/yet_again.erl
@@ -1,6 +1,7 @@
 -module(yet_again).
 -export([another_factorial/1]).
 -export([another_fib/1]).
+-export([power/2]).
 
 another_factorial(0) -> 1;
 another_factorial(N) -> N * another_factorial(N-1).
@@ -8,3 +9,7 @@ another_factorial(N) -> N * another_factorial(N-1).
 another_fib(0) -> 1;
 another_fib(1) -> 1;
 another_fib(N) -> another_fib(N-1) + another_fib(N-2).
+
+power(N,0) -> 1;
+power(N,1) -> N;
+power(N,M) -> N * power(N,M-1).
