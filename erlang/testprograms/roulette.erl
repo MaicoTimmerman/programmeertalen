957dccb079abc122347c073adb1c612d35728de2
Maico Timmerman <maico.timmerman@gmail.com>
2013-11-21 14:17:03 +0100

    Just testing.

--- erlang/testprograms/roulette.erl
+++ erlang/testprograms/roulette.erl
@@ -1,10 +1,11 @@
 %roulette.erl
 -module(roulette).
 -export([loop/0]).
-
 %send a number, 1-6
 loop() ->
-	receive
-		3-> io: format(“bang,~n”), exit({roulette, die, at, erlang:time()});
-		_ ->io: format(“click~n”), loop()
-end.
+    receive
+        3-> io: format("bang!~n"), 
+            exit({roulette, die, at, erlang:time()});
+        _ ->io: format("click~n"), 
+            loop()
+    end.
