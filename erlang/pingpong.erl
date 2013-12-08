9b1cd6aeb5f50b51668c4c792b6aa7741f9225a7
Maico Timmerman <maico.timmerman@gmail.com>
2013-12-08 21:26:28 +0100

    Reference for communicating processes.

--- erlang/pingpong.erl
+++ erlang/pingpong.erl
@@ -1,21 +1,23 @@
 -module(tut).
 -export([start/0, ping/1, pong/0]).
+
 ping(0) ->
     pong! finished,
-    io:format(“ping finished ~n”,[]);
+    io:format("ping finished ~n",[]);
 ping(N) ->
     pong! {ping, self()},
     receive
         pong ->
-            io:format(“Ping received pong~n”,[])
+            io:format("ping received pong~n",[])
     end,
     ping(N-1).
+
 pong() ->
     receive
         finished->
-            io:format(“Pong finished~n”, []);
+            io:format("pong finished~n", []);
         {ping, Ping_PID} ->
-            io:format(“Pong received ping~n”, []),
+            io:format("Pong received ping~n", []),
             Ping_PID ! pong,
             pong()
     end.
