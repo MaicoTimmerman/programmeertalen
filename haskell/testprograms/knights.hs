b787079ad911aa57b32c97a07605cb6f973b862b
Maico Timmerman <maico.timmerman@gmail.com>
2013-12-11 08:45:26 +0100

    Minor changes.

--- haskell/testprograms/knights.hs
+++ haskell/testprograms/knights.hs
@@ -1,9 +1,6 @@
 module Main where
-   data Islander = Knight | Knave deriving (Eq, Show)
-   john :: (Islander, Islander) -> Bool
-   john (x,y) = (x,y) == (Knave, Knave)
-  
-   solution3 :: [(Islander, Islander)]
-   solution3 = [ (x,y) | x <- [Knight, Knave],
-                         y <- [Knight, Knave],
-                         john(x,y) == (x == Knight) ]
+    data Islander = Knight | Knave deriving (Eq, Show)
+    john :: (Islander, Islander) -> Bool
+    john (x,y) = (x,y) == (Knave, Knave)
+    solution3 :: [(Islander, Islander)]
+    solution3 = [ (x,y) | x <- [Knight, Knave], y <- [Knight, Knave],john(x,y) == (x == Knight) ]
